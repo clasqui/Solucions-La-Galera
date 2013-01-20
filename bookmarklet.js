f7b50dd3b9aee82b489e53864c362e76e6d4b8e7
Marc Clascà <hulehule20@gmail.com>
2013-01-20 13:49:46 +0100

    Primer commit bookmarklet

---
+++ bookmarklet.js
@@ -0,0 +1,21 @@
+// Codi del Bookmarklet "Solucions La Galera Text (Autocorrectors)"
+// Autor: Marc Clascà
+// (c)2013
+// Visiteu pàgina oficial per instalació i més informació mclasca.sytes.net/solucions/
+
+
+(function () {
+	
+    flash = document.getElementsByTagName('object');
+
+    dataurl = flash[0].getAttribute("data");
+
+    dataurlsp = dataurl.replace("profesor=0", "profesor=1");
+
+    flash[0].setAttribute("data", dataurlsp);
+
+    param = flash[0].getElementsByTagName("param");
+
+    param[2].setAttribute("value", dataurlsp);
+
+})()

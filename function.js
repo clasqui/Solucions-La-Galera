a5c51ca0aca8ca034ae9dda29945b4271421244f
Marc Clascà <hulehule20@gmail.com>
2013-05-22 10:23:08 +0200

    Afegit debug a la consola

--- function.js
+++ function.js
@@ -1,24 +1,31 @@
-// Codi del la funci� "Solucions La Galera Text (Autocorrectors)"
-// Autor: Marc Clasc�
+// Codi del la funció "Solucions La Galera Text (Autocorrectors)"
+// Autor: Marc Clascà
 // (c)2013
-// Visiteu p�gina oficial per instalaci� i m�s informaci� mclasca.sytes.net/solucions/
+// Visiteu pàgina oficial per instalació i més informació mclasca.sytes.net/solucions/
 
 
 (function () {
+    console.log("Script iniciat");
 	
     flash = document.getElementsByTagName('object');
 
     dataurl = flash[0].getAttribute("data");
 
-    dataurlsp = dataurl.replace("profesor=0", "profesor=1");
+    if(!dataurlsp = dataurl.replace("profesor=0", "profesor=1")){
+    	console.log("No s'ha pogut canviar variable profesor.");
+    };
 
-    flash[0].setAttribute("data", dataurlsp);
+    if(!flash[0].setAttribute("data", dataurlsp)){
+    	console.log("No s'ha pogut aplicar canvi de variables al flash.")
+    };
 
     param = flash[0].getElementsByTagName("param");
 
     param[2].setAttribute("value", dataurlsp);
-    
+    console.log("Obrint finestra nova...")
     activitat = window.location;
     
-    window.open(activitat, "Apunta Aqu� les solucions!");
+    if(!window.open(activitat, "Apunta Aquí les solucions!")){
+    	console.log("No s'ha pogut obrir la finestra.")
+    };
 })()

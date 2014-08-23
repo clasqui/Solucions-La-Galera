cebdacec4467b603fbd509c7dabc974fe1f78c00
Marc Clascà <hulehule20@gmail.com>
2014-08-23 17:11:20 +0200

    Raw github chrome workaround

--- bookmarklet.js
+++ bookmarklet.js
@@ -4,4 +4,4 @@
 // Visiteu pàgina oficial per instalació i més informació mclasca.sytes.net/solucions/
 
 
-javascript:void((function(d){var e=d.createElement('script');e.setAttribute('type','text/javascript');e.setAttribute('charset','UTF-8');e.setAttribute('src','//raw.github.com/hulehule20/Solucions-La-Galera/master/function.js');d.body.appendChild(e)})(document));
+javascript:void((function(d){var e=d.createElement('script');e.setAttribute('type','text/javascript');e.setAttribute('charset','UTF-8');e.setAttribute('src','//rawgit.com/hulehule20/Solucions-La-Galera/master/function.js');d.body.appendChild(e)})(document));
